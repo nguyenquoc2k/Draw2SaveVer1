b0ae76980b652aac37a82e8030644195e4b3426e
ADMIN <nguyendinhquoc2000@gmail.com>
2024-12-04 16:49:30 +0700

    first commit

---
+++ Assets/MainGame/22.SaveDog/Scripts/Line.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public Rigidbody2D rd;
+
+    public List<Vector2> points = new List<Vector2>();
+    public int pointsCount = 0;
+
+    float pointsMinDistance = 0.1f;
+
+    //Circle collider added to each line's point
+    float circleColliderRadius;
+
+    public void AddPoint(Vector2 newPoint)
+    {
+        if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance)
+            return;
+
+        points.Add(newPoint);
+        pointsCount++;
+
+        //Add Circle Collider to the Point
+        CircleCollider2D circleCollider = this.gameObject.AddComponent<CircleCollider2D>();
+        circleCollider.offset = newPoint;
+        circleCollider.radius = circleColliderRadius;
+
+        //line renderer
+        lineRenderer.positionCount = pointsCount;
+        lineRenderer.SetPosition(pointsCount - 1, newPoint);
+
+        //edge collider
+        if (pointsCount > 1)
+        {
+            edgeCollider.points = points.ToArray();
+        }
+    }
+
+    public Vector2 GetLastPoint()
+    {
+        return (Vector2)lineRenderer.GetPosition(pointsCount - 1);
+    }
+
+    public void UsePhysics(bool usePhysics)
+    {
+        rd.isKinematic = !usePhysics;
+    }
+
+    public void SetLineColor(Gradient colorGradient)
+    {
+        lineRenderer.colorGradient = colorGradient;
+    }
+
+    public void SetPointMinDistance(float distance)
+    {
+        pointsMinDistance = distance;
+    }
+
+    public void SetLineWidth(float width)
+    {
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+
+        circleColliderRadius = width / 2f;
+        edgeCollider.edgeRadius = width / 2f;
+    }
+
+}
